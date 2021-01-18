2e26def78bd3bbe195ee9a7177cd851ebb820134
borg.lojasiewicz <borg.lojasiewicz@lekta.ai>
2021-01-18 15:40:40 +0100

    1 module w/ executable jar (no spring-boot!!!)

---
+++ controller/src/main/java/com/mapdecorator/MapDecoratorApplication.java
@@ -0,0 +1,13 @@
+package com.mapdecorator;
+
+/**
+ * Hello world!
+ *
+ */
+public class MapDecoratorApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
